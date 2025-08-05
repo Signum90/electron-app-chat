3527f9d3a26289aaec20ed49b7de327cfde3316c
Signum90 <signum.h1990@gmail.com>
2025-08-05 14:04:46 -0500

    comunicando front con back

--- main.js
+++ main.js
@@ -3,7 +3,14 @@
 ============================================================ */
 /**@DESC: xxxxxxxxxx */
 //# IMPORTS >
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
+/**
+ * ipcMain -> Permite comunicar el proceso principal
+ * con las vistas , eventos y mensajes, Se comunica de forma asincrónica desde el proceso principal a los procesos de renderizado.
+ * -------------------------------------
+ * ipcRenderer -> El módulo ipcRenderer es un EventEmitter. Proporciona un par de métodos para enviar mensajes sincrónicos y asincrónicos desde el proceso de renderizado (página web) al proceso principal.
+ */
+
 //# FUN: Crear Ventana >
 const createWindow = () => {
   // Instacia Ventana
@@ -11,11 +18,35 @@ const createWindow = () => {
     width: 1000,
     height: 600,
     // resizable: false // Evitar Reajustar el tamaño
+    // Realizar Integración con NODE, solo para desarrollo
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
   // Archivo HTML a cargar en ventana
   win.loadFile('./index.html');
   // Abrir las dev tools
   // win.webContents.openDevTools()
+  /**EVENTOS: En electro existen multiples eventos, cada clase u objeto tiene sus propios eventos
+   * el web content permite ejecutar eventos relativos a las pagias web de la aplicación
+   *   win.webContents.on('event-name', () => {})
+   */
+  // EVENTOS: 
+  win.webContents.on('did-finish-load', () => {
+    console.log("Finalizo la carga de la página")
+    
+    // Enviar mensaje a la página que lo invoca
+    win.webContents.send('data-from-server', 'Hola Página')
+  })
+  // ESCUCHAR EVENTOS DEL PROCESO DE RENDERIZADO
+  ipcMain.on('data-from-web', (event, data) => {
+    console.log("La página web dice", data);
+  })
+  ipcMain.on('data-from-web-click', (event, payload) => {
+    console.log('La página envia: ', payload);
+    
+  })
 }
 
 //# ARRANQUE >
